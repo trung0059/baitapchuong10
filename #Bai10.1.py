b90750a0f2accb3f7e1501ea36a02ffcf2dbdff2
trung0059 <117962365+trung0059@users.noreply.github.com>
2022-12-07 12:50:01 +0700

    Add files via upload

---
+++ #Bai10.1.py
@@ -0,0 +1,7 @@
+#Bai10.1
+a = int(input("nhập giá trị a: "))
+b = int(input("nhập giá trị b: "))
+c = int(input("nhập giá trị c: "))
+d = int(input("nhập giá trị d: "))
+j = [a,b,c,d]
+print("giá trị lớn nhất tỏng 4 giá trị vừa nhập là: ",max(j))
---
+++ #Bai10.3.py
@@ -0,0 +1,5 @@
+#Bai10.3 
+x = int(input("nhập x: "))
+n = int(input("nhập n: "))
+S = (pow(x,x)+1)*n
+print("giá trị của biểu thức là: ",S)
