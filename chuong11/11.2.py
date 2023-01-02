c63b35dc85bfe3afd3785b943501c8d076264801
trung0059 <117962365+trung0059@users.noreply.github.com>
2023-01-02 19:02:00 +0700

    Delete chuong11 directory

--- chuong11/11.2.py
+++
@@ -1,3 +0,0 @@
-teams=[['steven',"Neena","Lex","alexander","bruce"],['David',"Jack","Bill","tom","Mike","daniel"],["alexander","adam","payam","kevin","sigal","mike"]]
-worstteam=teams[2]
-print(worstteam[0])
